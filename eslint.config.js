34d4c6841a222d75bccdcfeca3717cdeea4a3272
Mister-Hope <mister-hope@outlook.com>
2025-02-23 12:30:42 +0800

    chore: tweaks

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,7 @@ import { hope } from "eslint-config-mister-hope";
 export default hope({
   ignores: ["docs/**"],
   ts: {
-    parserOptions: {
-      projectService: true,
-    },
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "no-console": "off",
-    },
+    "@typescript-eslint/no-explicit-any": "off",
+    "no-console": "off",
   },
 });
